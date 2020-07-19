a045f885e198f491aed1609197bb0f2e961069c4
Viktor Ponomarenko <v.ponomarenko@traderevolution.com>
2020-07-19 16:02:12 +0300

    Add SQLLite project

---
+++ UWPExamples/SqlLite/Customer.cs
@@ -0,0 +1,18 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLite
+{
+    public class Customer
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
