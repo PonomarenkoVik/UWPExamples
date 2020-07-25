2bda03e0e047e8dbc73d5a17869913465cd0c246
Viktor Ponomarenko <v.ponomarenko@traderevolution.com>
2020-07-25 14:58:18 +0300

    done MyBackgroundTask

---
+++ UWPExamples/BackgroundTasks/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BackgroundTasks
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void PlayButtonClick(object sender, RoutedEventArgs e)
+        {
+            //var task = new BackgroundTaskBuilder()
+            //{
+            //    Name = "MyTask",
+            //    TaskEntryPoint = typeof(MyBackgroundTask.MyTask).ToString()
+            //};
+
+            //var trigger = new ApplicationTrigger();
+            //task.SetTrigger(trigger);
+            //task.Register();
+            //await trigger.RequestAsync();
+
+            string myTaskName = "MyBackgroundTask";
+
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            { 
+                if (task.Value.Name == myTaskName)
+                {
+                    await (new MessageDialog("Task already registered")).ShowAsync();
+                    return;
+                }
+            }
+
+            await BackgroundExecutionManager.RequestAccessAsync();
+            BackgroundTaskBuilder taskBuilder = new BackgroundTaskBuilder()
+            {
+                Name = "SecondTask",
+                TaskEntryPoint = "MyBackgroundTask.MyTask"
+            };
+
+            taskBuilder.SetTrigger(new TimeTrigger(15, true));
+            BackgroundTaskRegistration mytask = taskBuilder.Register();
+
+            await (new MessageDialog("Task Registered")).ShowAsync();
+
+        }
+    }
+}
---
+++ UWPExamples/MyBackgroundTask/MyTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+
+namespace MyBackgroundTask
+{
+    public sealed class MyTask : IBackgroundTask
+    {
+
+        private BackgroundTaskDeferral _defferal;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _defferal = taskInstance.GetDeferral();
+            ShowToastNotification("Hello", "Hi this my first toast!");
+            _defferal.Complete();
+        }
+
+        private void ShowToastNotification(string title, string content)
+        {
+            ToastNotifier notifier = ToastNotificationManager.CreateToastNotifier();
+            Windows.Data.Xml.Dom.XmlDocument toastDoc = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+            XmlNodeList toastNodeList = toastDoc.GetElementsByTagName("text");
+            toastNodeList.Item(0).AppendChild(toastDoc.CreateTextNode(title));
+            toastNodeList.Item(1).AppendChild(toastDoc.CreateTextNode(content));
+
+            ToastNotification toast = new ToastNotification(toastDoc);
+            toast.ExpirationTime = DateTime.Now.AddSeconds(4);
+
+        }
+    }
+}
