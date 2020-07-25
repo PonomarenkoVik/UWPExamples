526bd2bc7bb48bf7e995c5887ce3b3a2a4a6e207
Viktor Ponomarenko <v.ponomarenko@traderevolution.com>
2020-07-25 17:52:20 +0300

    Add MediaApp

---
+++ UWPExamples/AudioPlayBackground/AudioPlay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace AudioPlayBackground
+{
+    public sealed class AudioPlay : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _defferal;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _defferal = taskInstance.GetDeferral();
+            taskInstance.Task.Completed += Task_Completed;
+        }
+
+        private void Task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            _defferal.Complete();
+        }
+    }
+}
